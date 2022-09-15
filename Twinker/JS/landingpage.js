558a3a82d3c2767b92a12e4e790db5109472892a
3vomanx <97153803+3vomanx@users.noreply.github.com>
2022-09-15 12:46:55 +0400

    Add files via upload

---
+++ Twinker/JS/landingpage.js
@@ -0,0 +1,9 @@
+hideDialog = () => {
+    var dialog = document.getElementById("mainpg_content");
+    dialog.style.display = "none"
+}
+
+ShowDialog = () => {
+    var dialog = document.getElementById("mainpg_content");
+    dialog.style.display = "flex"
+}
